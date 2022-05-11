825d392b9f4cad4e5071692fe1a9ebbeb3cc39e5
Geoffrey Tan <contact@geoffreytan.dev>
2022-05-11 17:46:30 +0200

    chore: add linting

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['airbnb-typescript', 'plugin:@typescript-eslint/recommended'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+      },
+    },
+    {
+      files: ['**/*'],
+      extends: ['prettier'],
+    },
+  ],
+  rules: {},
+}
